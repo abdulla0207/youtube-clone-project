0f6d8fc39a8109a88e8ba84113a92d89f5daa29b
abdulla0207 <abdulla.ermatov0407@gmail.com>
2022-12-21 14:46:33 +0500

    Creating packages(enums, exceptions, service)

---
+++ src/main/java/com/example/enums/ProfileRole.java
@@ -0,0 +1,8 @@
+package com.example.enums;
+
+public enum ProfileRole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR,
+    ROLE_PUBLISHER
+}
---
+++ src/main/java/com/example/exceptions/ItemNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.exceptions;
+
+public class ItemNotFoundException extends RuntimeException{
+
+
+}
