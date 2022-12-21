c244fc72e16d584707e875218f9fc199fb943300
G O L・D <imBekhruz>
2022-12-21 16:20:36 +0500

    ChannelEntity

--- src/main/java/com/example/entity/ChannelEntity.java
+++ src/main/java/com/example/entity/ChannelEntity.java
@@ -1,4 +1,14 @@
 package com.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jdk.jfr.Enabled;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "channel")
 public class ChannelEntity {
 }
