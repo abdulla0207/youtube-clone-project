7825049c6e18c6e1f439bb7d90f623aab0f7b91a
ACER <nurlanovicpdp@gmail.com>
2022-12-21 15:12:33 +0500

    Attach entityni yozip qoydim

---
+++ src/main/java/com/example/entity/AttachEntity.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "attach")
+public class AttachEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(name = "origin_name")
+    private String originName ;
+    @Column
+    private Integer size;
+    @Column
+    private String type;
+    @Column
+    private String path;
+    @Column
+    private String duration;
+}
