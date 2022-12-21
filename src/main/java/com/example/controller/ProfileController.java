0bb1806c974aa3b312ff50b9257879ca8449b340
G O L・D <imBekhruz>
2022-12-21 15:52:35 +0500

    first commit

--- src/main/java/com/example/controller/ProfileController.java
+++ src/main/java/com/example/controller/ProfileController.java
@@ -1,4 +1,7 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProfileController {
 }
--- src/main/java/com/example/dto/ProfileDTO.java
+++ src/main/java/com/example/dto/ProfileDTO.java
@@ -1,4 +1,18 @@
 package com.example.dto;
 
+import com.example.enums.ProfileRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProfileDTO {
+    private Integer id;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String attachId;
+    private ProfileRole role;
 }
